fa745495f3bd0a57b467ea98a19b5b12e5f80605
Sowmyasri11 <yenumulasowmya11@gmail.com>
2026-03-12 11:36:38 +0530

    UC1: Calculate length of a line using OOP

---
+++ src/Line.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+
+public class Line {
+
+    private Point p1;
+    private Point p2;
+
+    public Line(Point p1, Point p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    public double calculateLength() {
+
+        double x1 = p1.getX();
+        double y1 = p1.getY();
+        double x2 = p2.getX();
+        double y2 = p2.getY();
+
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.awt.*;
+
 public class Main {
+
     public static void main(String[] args) {
+
         System.out.println("Welcome to Line Comparison Computation Program");
+
+        Point p1 = new Point(2, 3);
+        Point p2 = new Point(4, 5);
+
+        Line line = new Line(p1, p2);
+
+        double length = line.calculateLength();
+
+        System.out.println("Length of Line : " + length);
     }
 }
